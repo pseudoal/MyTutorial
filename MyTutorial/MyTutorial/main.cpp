651589bd5e44ba450bcc657bea8359bfcbf2fd76
Alan <alan@alans-mbp.lan>
2018-06-03 14:37:09 +0200

    Added simple math example.

--- MyTutorial/MyTutorial/main.cpp
+++ MyTutorial/MyTutorial/main.cpp
@@ -7,9 +7,65 @@
 //
 
 #include <iostream>
+#include "main.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int valX, valY;
+    
+    std::cout << "Enter first value: ";
+    std::cin >> valX;
+    std::cout << "Enter second value: ";
+    std::cin >> valY;
+    
+    int resAdd = MyClass::AddFunc(valX, valY);
+    int resSubtract = MyClass::SubtractFunc(valX, valY);
+    int resMultiply = MyClass::MultiplyFunc(valX, valY);
+    double resDivide = MyClass::DivideFunc((double) valX, (double) valY);
+    
+    std::cout << valX << " + " << valY << " = " << resAdd << "\n";
+    std::cout << valX << " - " << valY << " = " << resSubtract << "\n";
+    std::cout << valX << " * " << valY << " = " << resMultiply << "\n";
+    if (resDivide == 0)
+    {
+        std::cout << valX << " / " << valY << " = " << "N/A" << "\n";
+    }
+    else
+    {
+         std::cout << valX << " / " << valY << " = " << resDivide << "\n";
+    }
+    
     return 0;
+}
+
+/**
+ * Comment.
+ * @return x and y added together.
+ */
+int MyClass::AddFunc(int x, int y)
+{
+    return x + y;
+}
+
+int MyClass::SubtractFunc(int x, int y)
+{
+    return x - y;
+}
+
+int MyClass::MultiplyFunc(int x, int y)
+{
+    return x * y;
+}
+
+double MyClass::DivideFunc(double x, double y)
+{
+    if (y != 0)
+    {
+        return x / y;
+    }
+    else
+    {
+        std::cout << "Error: divide by zero\n";
+        return 0;
+    }
 }
---
+++ MyTutorial/MyTutorial/main.h
@@ -0,0 +1,21 @@
+//
+//  main.h
+//  MyTutorial
+//
+//  Created by Alan on 2018-06-03.
+//  Copyright © 2018 Alan. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+class MyClass
+{
+public:
+    static int AddFunc(int x, int y);
+    static int SubtractFunc(int x, int y);
+    static int MultiplyFunc(int x, int y);
+    static double DivideFunc(double x, double y);
+};
+
+#endif /* main_h */
